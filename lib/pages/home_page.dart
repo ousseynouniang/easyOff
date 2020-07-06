cbc7aced84a34f08ff0157a9dfbba2bd0a55103c
Ousseynou NIANG <weuss.niang@gmail.com>
2020-07-06 14:43:21 +0000

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,55 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        color: Colors.white,
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'EasyFix Home Page',
+              style: TextStyle(color: Colors.black),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        color: Colors.blue,
+        backgroundColor: Colors.white,
+        buttonBackgroundColor: Colors.blue,
+        height: 60,
+        items: <Widget>[
+          Icon(Icons.home, size: 20, color: Colors.white),
+          Icon(Icons.add, size: 20, color: Colors.white),
+          Icon(Icons.shopping_cart, size: 20, color: Colors.white),
+          Icon(Icons.favorite, size: 20, color: Colors.white),
+        ],
+        animationDuration: Duration(milliseconds: 200),
+        index: 2,
+        animationCurve: Curves.bounceInOut,
+        onTap: (index) {
+          debugPrint("Current index is $index");
+        },
+      ),
+    );
+  }
+}
